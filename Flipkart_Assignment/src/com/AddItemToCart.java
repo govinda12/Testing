2a013202e629f2829f25f348060b731a507274d8
Chiranjeevi Aggarwal <chiranjeevi.aggarwal@gspann.com>
2016-08-24 12:32:35 +0530

    My first commit

---
+++ Flipkart_Assignment/src/com/AddItemToCart.java
@@ -0,0 +1,72 @@
+package com;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class AddItemToCart {
+	WebDriver driver;
+
+
+	@BeforeTest
+	public void setup(){
+		/*if(browser.equals("chrome")){
+			System.setProperty("webdriver.chrome.driver", "C:/Users/Admin/Downloads/Chrome2.23/chromedriver_win32/chromedriver.exe");
+			driver = new ChromeDriver();
+		}*/
+		//		if(browser.equals("firefox")){
+		driver = new FirefoxDriver();
+		//		}
+
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.get("https://www.flipkart.com/#");
+	}
+
+	@Test
+	public void addItemToBag() throws InterruptedException{
+		int randomSearchListRow;
+		int randomJeans;
+		int randomSizeJeansValue;
+		driver.findElement(By.xpath("//input[@placeholder='Search for Products, Brands and More']")).sendKeys("jeans");
+		driver.findElement(By.xpath("//button[@type='submit']")).click();
+
+		int searchListSize = driver.findElements(By.xpath("//div[@class='_2xw3j-']/div[3]/div")).size();
+		Random pickRandomSearchListRow = new Random();
+		randomSearchListRow = pickRandomSearchListRow.nextInt(searchListSize);
+		if(randomSearchListRow == 0){
+			randomSearchListRow=randomSearchListRow+1;
+		}
+
+		int noOfJeans=driver.findElements(By.xpath("//div[@class='_2xw3j-']/div[3]/div["+randomSearchListRow+"]/div")).size();
+		Random pickRandomJeans = new Random();
+		randomJeans = pickRandomJeans.nextInt(noOfJeans);
+		if(randomJeans == 0){
+			randomJeans=randomJeans+1;
+		}
+		driver.findElement(By.xpath("//div[@class='_2xw3j-']/div[3]/div["+randomSearchListRow+"]/div["+randomJeans+"]")).click();
+
+		Random random3 = new Random();
+		int jeansSizesCount = driver.findElements(By.xpath("//div[@class='rPoo01']//ul//a")).size();
+		randomSizeJeansValue = random3.nextInt(jeansSizesCount);
+		if(randomSizeJeansValue == 0){
+			randomSizeJeansValue=randomSizeJeansValue+1;
+		}
+
+		driver.findElement(By.xpath("//div[@class='rPoo01']//ul/li["+randomSizeJeansValue+"]/a")).click();
+		driver.findElement(By.xpath("//button[@class='_3zLR9i _3Plo8Q _19RW-r']")).click();
+		driver.findElement(By.xpath("//a[@class='_3NFO0d']")).click();
+		driver.findElement(By.xpath("//a[@title='Remove Item']")).click();
+	}
+
+	@AfterTest
+	public void quit(){
+		driver.quit();
+	}
+}
---
+++ gmail_Login/src/com/composeMail.java
@@ -0,0 +1,49 @@
+package com;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class composeMail {
+	
+	WebDriver driver;
+	
+	@BeforeTest
+	public void setup(){
+//		driver = new FirefoxDriver();
+		System.setProperty("webdriver.chrome.driver", "C:/Users/Admin/Downloads/Chrome2.23/chromedriver_win32/chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.get("https://www.google.com/intl/en/mail/help/about.html");
+	}
+	
+	@Test
+	public void loginGmail() throws InterruptedException{
+		driver.findElement(By.xpath("//a[text()='Sign in']")).click();
+		driver.findElement(By.id("Email")).sendKeys("chiranjeeviaggarwal");
+		driver.findElement(By.id("next")).click();
+		driver.findElement(By.id("Passwd")).sendKeys("govindagovinda");
+		driver.findElement(By.id("signIn")).click();
+		driver.findElement(By.xpath("//div[text()='COMPOSE']")).click();
+		driver.findElement(By.xpath("//textarea[@aria-label='To']")).sendKeys("shubham.mathur@gspann.com");
+		driver.findElement(By.xpath("//input[@placeholder='Subject']")).sendKeys("Automated Mail");
+		driver.findElement(By.xpath("//input[@placeholder='Subject']")).sendKeys(Keys.TAB);
+		driver.findElement(By.xpath("//div[@aria-label='Message Body']")).sendKeys("Thank u for Monsoon Selenium Training !!!!");
+		driver.findElement(By.xpath("//div[text()='Send']")).sendKeys("Automated Mail");
+		Thread.sleep(10000);
+	}
+	
+	@AfterTest
+	public void quit(){
+		driver.quit();
+	}
+}
